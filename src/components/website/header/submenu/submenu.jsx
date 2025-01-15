5dad36a1ad7c6d65cca33ee18c59d038b4d9daa6
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2025-01-15 06:10:33 +0500

    header updated

--- src/components/website/header/submenu/submenu.jsx
+++ src/components/website/header/submenu/submenu.jsx
@@ -7,6 +7,7 @@ import Image from "next/image"
 import Mockup from "media/home/services/promo.png"
 // Css
 import style from "./submenu.module.css"
+import { ArrowBigRight } from "lucide-react"
 
 const servicesOne = [
   {
@@ -28,7 +29,10 @@ const servicesOne = [
   {
     text: "News / Article Publishing",
     href: "/news-article-publishing",
-  },
+  }
+]
+
+const servicesTwo = [
   {
     text: "Paid Ad Campaigns",
     href: "/paid-ad-campaigns",
@@ -37,9 +41,6 @@ const servicesOne = [
     text: "Influencer Marketing",
     href: "/influencer-marketing",
   },
-]
-
-const servicesTwo = [
   {
     text: "Search Engine Optimization",
     href: "/search-engine-optimization",
@@ -51,7 +52,9 @@ const servicesTwo = [
   {
     text: "Print Placement (Magazine)",
     href: "/print-placement",
-  },
+  }
+]
+const servicesThree = [
   {
     text: "Social Media Marketing",
     href: "/social-media-marketing",
@@ -67,78 +70,75 @@ const servicesTwo = [
   {
     text: "TV Interviews",
     href: "/tv-interviews",
-  }
+  },
 ]
 
 export default function Submenu() {
   return (
     <div className={style.root}>
       <div className={style.left}>
-        <div>
-          <ul className={style.navlinks}>
-            {servicesOne.map(({ text, href }) => (
-              <NavigationMenuItem key={text}>
-                <Link href={href} legacyBehavior passHref>
-                  <NavigationMenuLink>
-                    <span dangerouslySetInnerHTML={{ __html: text }} />
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            ))}
-          </ul>
+        <div className={style.navText}>
+          <h3 className={style.title}>We're more than Survey Softwere</h3>
+          <p className={style.para}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit
+            corporis hic laudantium optio, fugiat molestiae culpa itaque neque
+            perspiciatis. Reprehenderit?
+          </p>
         </div>
-        <div>
-          <ul className={style.navlinks}>
-            {servicesTwo.map(({ text, href }) => (
-              <NavigationMenuItem key={text}>
-                <Link href={href} legacyBehavior passHref>
-                  <NavigationMenuLink>
-                    <span dangerouslySetInnerHTML={{ __html: text }} />
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            ))}
-          </ul>
-        </div>
-        {/* <div>
-          <ul className={style.navlinks}>
-            {servicesThree.map(({ text, href }) => (
-              <NavigationMenuItem key={text}>
-                <Link href={href} legacyBehavior passHref>
-                  <NavigationMenuLink>
-                    <span dangerouslySetInnerHTML={{ __html: text }} />
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            ))}
-          </ul>
-        </div> */}
-        {/* <div>
-          <ul className={style.navlinks}>
-          {servicesFour.map(({ text, href }) => (
-            <NavigationMenuItem key={text}>
-                <Link href={href} legacyBehavior passHref>
-                  <NavigationMenuLink>
-                  <span dangerouslySetInnerHTML={{ __html: text }} />
-                  </NavigationMenuLink>
+        <div className={style.links}>
+          <div>
+            <ul className={style.navlinks}>
+              {servicesOne.map(({ text, href }) => (
+                <NavigationMenuItem key={text}>
+                  <Link href={href} legacyBehavior passHref>
+                    <NavigationMenuLink>
+                      <span dangerouslySetInnerHTML={{ __html: text }} />
+                    </NavigationMenuLink>
                   </Link>
-                  </NavigationMenuItem>
-                  ))}
-                  </ul>
-                  </div> */}
+                </NavigationMenuItem>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <ul className={style.navlinks}>
+              {servicesTwo.map(({ text, href }) => (
+                <NavigationMenuItem key={text}>
+                  <Link href={href} legacyBehavior passHref>
+                    <NavigationMenuLink>
+                      <span dangerouslySetInnerHTML={{ __html: text }} />
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <ul className={style.navlinks}>
+              {servicesThree.map(({ text, href }) => (
+                <NavigationMenuItem key={text}>
+                  <Link href={href} legacyBehavior passHref>
+                    <NavigationMenuLink>
+                      <span dangerouslySetInnerHTML={{ __html: text }} />
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
       <div className={style.right}>
-        <Image src={Mockup} alt="domain" />
         <div>
           <h2 className={style.title}>
-            Activate Your <span className="text-blue">Coupon</span>{" "}
-            <span className="text-pink">
-              {" "}
-              <br /> 50% off
-            </span>
+            Activate Your Coupon
+            <br /> 50% off
           </h2>
           <p className={style.para}>Get Affordable Rates</p>
+          <Image src={Mockup} alt="domain" width={350} height={350} />
         </div>
+      </div>
+      <div className={style.bottomBar}>
+        <Link href="/" className="flex items-center gap-x-3 justify-center">Explore Our New Survery Features <ArrowBigRight className="size-8"/></Link>
       </div>
     </div>
   )
